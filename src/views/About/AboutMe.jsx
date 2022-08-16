474471c4fb4c933d138cbd296ccafae506ae75dd
karyssa-dandrea <karyssadandrea@gmail.com>
2022-08-16 12:24:09 -0700

    changed about me and added book and spotify

--- src/views/About/AboutMe.jsx
+++ src/views/About/AboutMe.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import me from '../../assets/me.jpg';
 import styles from './About.css';
+import witch from '../../assets/witch.jpeg';
+import Spotify from 'react-spotify-embed';
 
 export default function AboutMe() {
   return (
     <div className={styles.about}>
-      <img src={me} className={styles.img} alt="selfie" />
       <div className={styles.aboutbio}>
         <p>
-          Thank you for being here! I am a software developer currently residing
-          in Visalia, CA. I got into software development in 2014 when I started
-          a job at a watch company doing customer service. Shortly after I got
-          promoted to be a Front End Developer and started using integrated HTML
-          and CSS. I loved being able to create something and see it come to
-          life and wanted more control over the process. I also received my
-          certificate in Commercial Music, which has allowed me to learn how to
-          use various software. I love learning and creating which has brought
-          me here.
+          <img src={me} className={styles.img} alt="selfie" />
+          Hello! My name is Karyssa and I enjoy creating things that live on the
+          web. My interest in web development started back in 2014 when I
+          started a job at a watch company where I got introduced to HTML and
+          CSS. I loved being able to create something and see it come to life
+          and wanted more control over the process. I also received my
+          Certificate in Commercial Music in 2020, which has allowed me to learn
+          and navigate various software. I love to learn and create!
         </p>
         <p>
-          I want to use software development to focus on user experience, use my
-          creativity, and it is just so much fun! I also love that software can
-          make a difference for others. I would love to work with you, or be
-          apart of your team!
+          Fast-forward to today, and I am now a Full Stack Software Developer!
+          Although I specialize in the Front End. My main focus these days is
+          building accessible, beautiful and user friendly digital experiences.
+          Let's work together!
         </p>
+
+        <h3> Currently Reading:</h3>
+        <img src={witch} className={styles.book} alt="image of book" />
+        <h3> On Repeat:</h3>
+        <div className={styles.spotify}>
+          <Spotify link="https://open.spotify.com/playlist/6tMR6bBpetCK0mHP5WfTwF?si=f379312a27b744f2" />
+        </div>
       </div>
     </div>
   );
