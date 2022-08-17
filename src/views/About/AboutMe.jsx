029e73fd57943cbcc3769ef632a1a6a125b42efd
karyssa-dandrea <karyssadandrea@gmail.com>
2022-08-17 15:54:09 -0700

    added grid to my about me section

--- src/views/About/AboutMe.jsx
+++ src/views/About/AboutMe.jsx
@@ -8,28 +8,35 @@ export default function AboutMe() {
   return (
     <div className={styles.about}>
       <div className={styles.aboutbio}>
-        <p>
-          <img src={me} className={styles.img} alt="selfie" />
-          Hello! My name is Karyssa and I enjoy creating things that live on the
-          web. My interest in web development started back in 2014 when I
-          started a job at a watch company where I got introduced to HTML and
-          CSS. I loved being able to create something and see it come to life
-          and wanted more control over the process. I also received my
-          Certificate in Commercial Music in 2020, which has allowed me to learn
-          and navigate various software. I love to learn and create!
-        </p>
-        <p>
-          Fast-forward to today, and I am now a Full Stack Software Developer!
-          Although I specialize in the Front End. My main focus these days is
-          building accessible, beautiful and user friendly digital experiences.
-          Let's work together!
-        </p>
-
-        <h3> Currently Reading:</h3>
-        <img src={witch} className={styles.book} alt="image of book" />
-        <h3> On Repeat:</h3>
-        <div className={styles.spotify}>
-          <Spotify link="https://open.spotify.com/playlist/6tMR6bBpetCK0mHP5WfTwF?si=f379312a27b744f2" />
+        <div className={styles.gridcontainer}>
+          <img src={me} className={styles.selfie} alt="selfie" />
+          <div className={styles.grid1}>
+            <p>
+              Hello! My name is Karyssa and I enjoy creating things that live on
+              the web. My interest in web development started back in 2014 when
+              I started a job at a watch company where I got introduced to HTML
+              and CSS. I loved being able to create something and see it come to
+              life and wanted more control over the process. I also received my
+              Certificate in Commercial Music in 2020, which has allowed me to
+              learn and navigate various software. I love to learn and create!
+            </p>
+            <p>
+              Fast-forward to today, and I am now a Full Stack Software
+              Developer! Although I specialize in the Front End. My main focus
+              these days is building accessible, beautiful and user friendly
+              digital experiences. Let's work together!
+            </p>
+          </div>
+          <div className={styles.grid2}>
+            <h3> Currently Reading:</h3>
+            <img src={witch} className={styles.book} alt="image of book" />
+          </div>
+          <div className={styles.grid3}>
+            <h3> On Repeat:</h3>
+            <div className={styles.spotify}>
+              <Spotify link="https://open.spotify.com/playlist/6tMR6bBpetCK0mHP5WfTwF?si=f379312a27b744f2" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,9 +5,9 @@ import styles from './Home.css';
 export default function Home() {
   return (
     <div className={styles.home}>
-      <img src={ryssa} className={styles.img} alt="karyssa logo" />
       <div className={styles.bio}>
         <p>
+          <img src={ryssa} className={styles.img} alt="karyssa logo" />
           Hello, welcome! <br />
           I'm Karyssa Dandrea. <br />
           I'm a full stack software developer specializing in the front end. I
