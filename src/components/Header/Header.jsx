934153a71758ce6f76bfbe5488ef7d5626f3c80a
karyssa-dandrea <karyssadandrea@gmail.com>
2022-09-09 14:49:49 -0700

    footer and header mobile friendky

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,33 +2,27 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.css';
 import resume from '../../assets/resume.png';
-import { useState } from 'react';
-// import Mobile from './Mobile';
 
 export default function Header() {
-  // const [open, setOpen] = useState(false);
-
-  // const buttonClick = (e) => {
-  //   setOpen(!open);
-  // };
   return (
-    <div className={styles.header}>
-      {/* <Mobile buttonClick={buttonClick} /> */}
-      <NavLink to="/">
-        <div className={styles.home}>Home</div>
-      </NavLink>
-      <NavLink to="/about">
-        <div className={styles.about}>About</div>
-      </NavLink>
-      <NavLink to={resume} target="_blank">
-        <div className={styles.resume}>Resume</div>
-      </NavLink>
-      <NavLink to="/projects">
-        <div className={styles.project}>Projects</div>
-      </NavLink>
-      <NavLink to="/tech">
-        <div className={styles.tech}>Tech Stack</div>
-      </NavLink>
-    </div>
+    <>
+      <div className={styles.header}>
+        <NavLink to="/">
+          <div className={styles.home}>Home</div>
+        </NavLink>
+        <NavLink to="/about">
+          <div className={styles.about}>About</div>
+        </NavLink>
+        <NavLink to={resume} target="_blank">
+          <div className={styles.resume}>Resume</div>
+        </NavLink>
+        <NavLink to="/projects">
+          <div className={styles.project}>Projects</div>
+        </NavLink>
+        <NavLink to="/tech">
+          <div className={styles.tech}>Tech Stack</div>
+        </NavLink>
+      </div>
+    </>
   );
 }
